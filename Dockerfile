42945024deb3679cb9fea917ea126baea7a6b4c0
Ellis Percival <flyte@failcode.co.uk>
2022-06-16 17:10:04 +0100

    Just check cert hash once per minute, since inotify events are weird for mounted secrets

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM alpine:latest
 
-RUN apk add --no-cache inotify-tools
-
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,13 @@ set -u
 cert_hash="$(sha256sum $cert_path | awk '{ print $1 }')"
 echo "$(date -u) - Current checksum '$cert_hash'"
 
-inotifywait -q -m -e modify -e attrib -e close_write "$cert_path" |
-  while read -r path action file; do
-    echo "Received action $action on $cert_path"
-    new_cert_hash="$(sha256sum $cert_path | awk '{ print $1 }')"
-    if [ "$cert_hash" != "$new_cert_hash" ]; then
-      echo "$(date -u) - New checksum '$new_cert_hash'"
-      cert_hash="$new_cert_hash"
-      echo "$(date -u) - Sending SIGHUP signal to Vault"
-      killall -SIGHUP "vault"
-    fi
-  done
+while [ 1 ]; do
+  new_cert_hash="$(sha256sum $cert_path | awk '{ print $1 }')"
+  if [ "$cert_hash" != "$new_cert_hash" ]; then
+    echo "$(date -u) - New checksum '$new_cert_hash'"
+    cert_hash="$new_cert_hash"
+    echo "$(date -u) - Sending SIGHUP signal to Vault"
+    killall -SIGHUP "vault"
+  fi
+  sleep 60
+done
